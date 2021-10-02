5ac7540798637c9e79f4fc25d722b023715b0bb4
Phyo Pyae Sone Han <68275444+phyohan18@users.noreply.github.com>
2021-10-02 20:17:13 +0630

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,12 +31,15 @@
                                 </th>
                             </tr>
                             </thead>
-
+    
                             <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                            @php
+                                $number = 1;
+                            @endphp
                             @foreach($categories as $category)
                                 <tr class="bg-white">
                                     <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900 text-center">
-                                        {{ $category->id }}
+                                        {{ $id++ }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900 text-center">
                                         {{ $category->name }}
